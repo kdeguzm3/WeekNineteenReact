a346cec494f1ef688b9cc38d18f9f9751d99276f
Cosplay Diver <kaydeekhing@gmail.com>
2020-06-06 19:43:18 -0400

    added find and sort functions

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,12 +1,28 @@
 import React, { useState } from 'react';
 
-const Options = ({query}) => {
+const Options = ({query, setQuery, sort, setSort, handleSubmit, queryType, setQueryType}) => {
     const [collapse, setCollapse] = useState (true)
+
 
 return (
     <div className = "optionsDiv">
-        <div className = "optionsTab"> FILTER/SORT </div>
-
+        <div className = "optionsTab" onClick = {() => setCollapse(!collapse)}> FILTER/SORT {collapse ? "" : "[X]"} </div>
+        <div className = {collapse ? "optionsForm collapsed" : "optionsForm"}><hr></hr>
+        <br />
+        <input type = "text" value = {query} onChange = {(e) => setQuery(e.target.value) }></input>
+        <br />
+        <label>FIND BY:</label> <br /> 
+        <select value = {queryType} onChange = {(e) => setQueryType (e.target.value)}>
+            <option value = "Name"> Name </option>
+            <option value = "Bending"> Bending </option>
+            <option value = "Organization"> Organization </option>
+        </select>
+        <br /><br /><br />
+        <label> SORT:</label><br></br>
+        <input type = "radio" name = "sorting" value = "Name" checked = {sort === "Name" ? true : false} onChange = {(e) => setSort(e.target.value)}></input> By Name <br></br>
+        <input type = "radio" name = "sorting" value = "Organization" checked = {sort === "Organization" ? true : false} onChange = {(e) => setSort(e.target.value)}></input> By Organization <br></br>
+        <button onClick = {handleSubmit}>Submit</button>
+        </div> 
     </div>
 )
 };
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,15 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Employee from './Employee';
 import EmployeeData from '../data/employees.json';
 import Options from './Options';
 
 const EmployeeTable = () => {
 
+    const [query, setQuery] = useState("(All)");
+    const [queryType, setQueryType] = useState("Name");
+    const [sort, setSort] = useState("name");
+    const [arrayResult, setArrayResult] = useState (EmployeeData);
+
+
+    const handleSubmit = () => {
+        let tempArray = EmployeeData;
+        tempArray = tempArray.filter((element) => {
+            if (query === "(All)" || element[queryType].includes(query)) {
+                return true; 
+            } else {return false}
+        })
+        tempArray.sort ((a,b) => a[sort] > b[sort] ? 1 : -1
+        )
+        console.log(tempArray);
+
+        setArrayResult (tempArray)
+    };
+
 return (
     <div>
         <div className = "Table">
             {
-                EmployeeData.map(
+                arrayResult.map(
                     (data, index) => {
                         return <Employee {...data} key = {index} />
                     }
@@ -17,7 +37,7 @@ return (
             }
 
         </div>
-        <Options />
+        <Options query = {query} setQuery = {setQuery} sort = {sort} setSort = {setSort} handleSubmit = {handleSubmit} queryType = {queryType} setQueryType = {setQueryType}/>
     </div>
 )
 };
